e282b15630be09674a6ad67727037834228f27da
Glitch (empty-glitch) <none>
2021-08-25 13:36:06 +0000

    👴📮 Checkpoint ./roles.json:27621106/56 ./users.json:27621106/79 ./src/db.js:27621106/59

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGO_URI || 'mongodb://localhost:27017/sarra-membership', { 
+  process.env.MONGO_URI || 'mongodb://localhost:4000/arras-tx-dashboard.glitch.me', { 
     useNewUrlParser: true, 
     useCreateIndex: true 
   }
