c6947b37550a8f83c58d0fb50ba4bf505c5a0a8c
Glitch (arras-tx-dashboard) <none>
2021-09-29 15:12:06 +0000

    🏡💄 Checkpoint ./src/db.js:27621106/773 ./users.json:27621106/953

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 
 mongoose.connect(
   process.env.MONGO_URI || 'mongodb+srv://admin69:arrastx_membership@arras0tx0membership.d9fx6.mongodb.net/arrastx_membership?retryWrites=true&w=majority', { 
@@ -7,4 +7,18 @@ mongoose.connect(
   }
 )
 
+module.exports = mongoose; */
+// here is where we connect to the database
+const mongoose = require('mongoose');
+const mongodb_URI =
+  "mongodb+srv://cs152ajSum2020:2emSqHY7pwyoHZ9k@cluster0.yjamu.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"; //process.env.MONGODB_URI
+//const mongodb_URI = 'mongodb+srv://tjhickey:WcaLKkT3JJNiN8dX@cluster0.kgugl.mongodb.net/atlasAuthDemo?retryWrites=true&w=majority' //process.env.MONGODB_URI
+const dbURL = mongodb_URI;
+mongoose.connect(dbURL, { useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("we are connected!!!");
+});
 module.exports = mongoose;
