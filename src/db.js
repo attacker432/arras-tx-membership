540a03f2fee90387bec036841eb6fe6decd1e4cc
Glitch (arras-tx-dashboard) <none>
2021-09-29 14:49:46 +0000

    💽💵 Checkpoint ./src/db.js:27621106/469 ./src/server.js:27621106/4 ./package.json:27621106/61

--- src/db.js
+++ src/db.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(
   process.env.MONGO_URI || 'mongodb://localhost:27017/sarra-membership', { 
     useNewUrlParser: true, 
-    useCreateIndex: true,
-    useUnifiedTopology: true
+    useCreateIndex: true 
   }
 )
 
