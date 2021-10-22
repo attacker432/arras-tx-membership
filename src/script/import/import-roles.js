6e7c5db9987e03b9e0df5f9fe109df7ae6e01118
Glitch (lurraxster-io) <none>
2021-10-22 13:06:59 +0000

    🎂😜 Checkpoint ./roles.json:27621106/150 ./src/script/import/import-roles.js:27621106/1236

--- src/script/import/import-roles.js
+++ src/script/import/import-roles.js
@@ -1,13 +1,21 @@
 const db = require('../../db');
 const Role = require('../../models/role');
 const roles = require('../../../roles.json');
+const ServerAudit = require('../../models/server-audit');
 
 (async function () {
   for (let i = 0; i < roles.length; i++) {
     try {
+      roles[i].username = roles[i].username.trim();
+
       const role = await Role.create(roles[i]);
-      console.log(`Role ${role.name} created.`);
-      
+      console.log(`User ${role.name} created.`);
+
+      await ServerAudit.create({
+        userid: null,
+        username: 'System',        
+        action: `System created role ${role.name}.`
+      });
     }
     catch (err){
       console.log(err);
