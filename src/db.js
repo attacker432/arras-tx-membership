0c92ff0ef6f169e55bf9190fc6c353673f13ec2f
Glitch (lurraxster-io) <none>
2021-10-23 19:33:16 +0000

    🎗💇 Checkpoint ./config.json:27621106/6 ./src/db.js:27621106/102

--- src/db.js
+++ src/db.js
@@ -11,7 +11,7 @@ module.exports = mongoose; */
 // here is where we connect to the database
 const mongoose = require('mongoose');
 const mongodb_URI =
-  "mongodb+srv://attacker:uarebald@cluster0.d7oj2.mongodb.net/Cluster0?retryWrites=true&w=majority"; //process.env.MONGODB_URI
+ process.env.MONGODB_URI
 //const mongodb_URI = 'mongodb+srv://tjhickey:WcaLKkT3JJNiN8dX@cluster0.kgugl.mongodb.net/atlasAuthDemo?retryWrites=true&w=majority' //process.env.MONGODB_URI
 const dbURL = mongodb_URI;
 mongoose.connect(dbURL, {
