d57d8a43436d271eb16bdb6de8586d44d44f448a
Glitch (membership-test-arras) <none>
2022-05-19 08:00:51 +0000

    Rewound to commit 0bfd4ffb315c8ca9f71fb0864509021b69497326 + 0 edits

--- src/globals.js
+++ src/globals.js
@@ -12,11 +12,11 @@ module.exports = {
     RoleNameRegEx: /^[a-zA-Z\(\)](\s?[a-zA-Z\(\)])+$/,
 
     DefaultRoleColor: '#ffffff',
-    DefaultRoleName: 'Member',
+    DefaultRoleName: 'Developer',
     DeveloperRole: "Developer",
     DeveloperRoleValue: 1000,
 
-    DefaultMemberStatus: "Inactive",
+    DefaultMemberStatus: "Active",
     ValidMemberStatuses: [
         "Active",
         "Inactive",
